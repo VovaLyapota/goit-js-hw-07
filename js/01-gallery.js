deb7efaf596167a59ee0a76d66a515568e53046b
VovaMyb <rimidalvatopayl@gmail.com>
2023-05-09 17:16:23 +0300

    add tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const listEls = document.querySelector(".gallery");
+
+const newItemsOfListEls = [];
+galleryItems.forEach((obj) => {
+  let newItem = createNewItem(obj);
+  newItemsOfListEls.push(newItem);
+});
+
+// Розумію, що можна було просто через innerHTML, але вирішив потренуавтись
+function createNewItem(obj) {
+  const newItem = document.createElement("li");
+  newItem.classList.add("gallery__item");
+
+  const newSourceOfImg = document.createElement("a");
+  newSourceOfImg.classList.add("gallery__link");
+
+  newSourceOfImg.href = obj.original;
+
+  const newImg = document.createElement("img");
+  newImg.classList.add("gallery__image");
+
+  newImg.dataset.source = obj.original;
+  newImg.src = obj.preview;
+  newImg.alt = obj.description;
+
+  newSourceOfImg.appendChild(newImg);
+  newItem.appendChild(newSourceOfImg);
+  return newItem;
+}
+
+listEls.append(...newItemsOfListEls);
+
+listEls.addEventListener("click", onImgSourceClick);
+
+function onImgSourceClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+        <img src="${event.target.dataset.source}">
+    `
+  );
+
+  instance.show();
+
+  listEls.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && instance.visible()) {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const listEls = document.querySelector(".gallery");
+
+const newItemsOfListEls = galleryItems
+  .map((obj) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${obj.original}">
+      <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+   </a>
+   </a>
+</li>`;
+  })
+  .join("");
+
+listEls.insertAdjacentHTML("beforeend", newItemsOfListEls);
+
+listEls.addEventListener("click", onImgSourceClick);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: ".gallery__image",
+  captionType: "inner",
+  captionsData: "alt",
+});
+
+function onImgSourceClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = event.target
+    .closest(".gallery__item")
+    .querySelector(".gallery__link").href;
+
+  const lightbox = new SimpleLightbox(selectedImage);
+}
